12066a84b681d93b2508099ff8f7eafd20f25c49
01587 <67268248+01587@users.noreply.github.com>
2020-07-06 10:19:34 +0200

    MOMO

---
+++ EX_01/afficher-parametres.php
@@ -0,0 +1,10 @@
+
+<?php
+foreach ($_GET as $key => $value){
+    echo "<ul>";
+    echo "<li>";
+    echo  " cle: " . $key .  " valeur: " .$value ;  
+    echo "</ul>";
+
+}
+?>
---
+++ EX_02/vitrine-contact.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="vitrine-accueil.html"> accueil</a>
+            <a href="vitrine-contact.html"> contact</a>
+            <a href="vitrine-programme.html"> programme</a>
+        </nav>
+    </header>
+    <h1>Body vitrine contact</h1>
+    <footer>
+        <h1>Footer vitrine contact</h1>
+    </footer>
+</body>
+
+</html>
